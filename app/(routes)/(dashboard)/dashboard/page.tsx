08a59a2af76d86e59a356c213257b19730f0f2a8
Luis Vanegas <lvanegas1429@gmail.com>
2024-10-04 11:32:47 -0600

    Sidebar and Navbar Dashboard done

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const DashboardPage = () => {
     return (
-        <div>DashboardPage</div>
+        <div>
+            <div className='flex justify-between'>
+                <h2 className='text-3xl font-bold'>List of cars</h2>
+            </div>
+            <p className='text-slate-500 text-muted-foreground text-sm mt-2'>Listado de coches</p>
+        </div>
     )
 }
 
